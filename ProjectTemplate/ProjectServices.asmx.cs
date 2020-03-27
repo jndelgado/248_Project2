76589b3bbfc7c89197622bdbd7ccc8e7d6957e22
jndelgado <jndelgad@asu.edu>
2020-03-26 20:10:57 -0700

    cleaned up file and added a tailwind test file

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MySql.Data.MySqlClient;
+using RestSharp;
+using System;
+using System.Data;
 using System.Web;
 using System.Web.Services;
-using MySql.Data;
-using MySql.Data.MySqlClient;
-using System.Data;
-using RestSharp;
-using System.Configuration;
-using System.Data.SqlClient;
 
 
 
